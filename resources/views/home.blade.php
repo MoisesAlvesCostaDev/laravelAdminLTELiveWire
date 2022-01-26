5d7c97eed51dc160b6759344e333ed13149be132
Moises Alves Costa <moi.ac@hotmail.com>
2022-01-26 20:37:33 -0300

    feat: Criação inicial do projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+
+    <div class="container">
+        <div class="row border ">
+            <div class="col">
+                ola
+            </div>
+            <div class="col">
+                ola
+            </div>
+
+        </div>
+
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
