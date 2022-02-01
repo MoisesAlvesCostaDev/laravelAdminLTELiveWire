5b73529f81bdfd553c98200db03397634f87053e
Moises Alves Costa <moi.ac@hotmail.com>
2022-02-01 20:59:44 -0300

    feat: dev front adicionar cliente

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -13,6 +13,6 @@ class UserController extends Controller
 {
     public function index()
     {
-        return view('admin.user.index');
+        return view('admin.user.create');
     }
 }
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -22,89 +22,82 @@
 
 @section('content')
     <div class="content">
-        <div class="container-fluid ">
-            <div class="bg-white shadow-sm rounded">
-                <div class="container-fluid ml-0 mr-0 pr-0 pl-0 border">
-                    <div class="row" style="height:57px">
-                        <div class="col-3 ml-2 d-flex align-items-center">
-                            <h5 class="p-0 m-0">Listagem de Usuários</h5>
-                        </div>
-                        <div class="col d-flex align-items-center justify-content-end">
-                            <div class="col-5">
-                                <div class="input-group">
-                                    <input type="text" class="form-control border-dark" placeholder="Nome do usuário">
-                                    <div class="input-group-append">
-                                        <span class="input-group-text bg-white border-dark">
-                                            <i class="fa fa-search "></i>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
+        <div class="container-fluid bg-white shadow-sm rounded border p-0">
+            <div class="card-header">
+                <h5 class="card-title wrapper">Listagem de usuários</h5>
+                <div class="card-tools">
+                    <div class="input-group input-group-sm" style="width: 150px;">
+                        <input type="text" name="table_search" class="form-control float-right"
+                            placeholder="Nome do usuário">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fas fa-search"></i>
+                            </button>
                         </div>
                     </div>
-                    <table class="table table-striped m-0 p-0 ">
-                        <thead>
-                            <tr>
-                                <th scope="col">id</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">E-mail</th>
-                                <th scope="col">Situação</th>
-                                <th scope="col" width="100">Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>
-                                    <span class="badge badge-success">Ativo</span>
-                                </td>
-                                <td class="text-nowrap">
-                                    <a data-tt="tooltip" data-placement="top" title="Ver detalhes"><i
-                                            class="fa fa-search m-0 p-0 ml-1 mr-1"></i></a>
-                                    <a data-tt="tooltip" data-placement="top" title="Editar usuário"><i
-                                            class="fas fa-edit m-0 p-0 ml-1 mr-1"></i></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td> <span class="badge badge-success">Ativo</span></td>
-                                <td class="text-nowrap">
-                                    <a data-tt="tooltip" data-placement="top" title="Ver detalhes"><i
-                                            class="fa fa-search m-0 p-0 ml-1 mr-1"></i></a>
-                                    <a data-tt="tooltip" data-placement="top" title="Editar usuário"><i
-                                            class="fas fa-edit m-0 p-0 ml-1 mr-1"></i></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td> <span class="badge badge-danger">inativo</span></td>
-                                <td class="text-nowrap">
-                                    <a data-tt="tooltip" data-placement="top" title="Ver detalhes"><i
-                                            class="fa fa-search m-0 p-0 ml-1 mr-1"></i></a>
-                                    <a data-tt="tooltip" data-placement="top" title="Editar usuário"><i
-                                            class="fas fa-edit m-0 p-0 ml-1 mr-1"></i></a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
                 </div>
-                <div class="clearfix mt-5 mr-3">
-                    <ul class="pagination pagination m-0 float-right">
-                        <li class="page-item"><a class="page-link" href="#">«</a></li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">»</a></li>
-                    </ul>
-                </div>
-                <br>
             </div>
+            <table class="table table-striped m-0 p-0 ">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">E-mail</th>
+                        <th scope="col">Situação</th>
+                        <th scope="col" width="100">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>
+                            <span class="badge badge-success">Ativo</span>
+                        </td>
+                        <td class="text-nowrap">
+                            <a data-tt="tooltip" data-placement="top" title="Ver detalhes"><i
+                                    class="fa fa-search m-0 p-0 ml-1 mr-1"></i></a>
+                            <a data-tt="tooltip" data-placement="top" title="Editar usuário"><i
+                                    class="fas fa-edit m-0 p-0 ml-1 mr-1"></i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td> <span class="badge badge-success">Ativo</span></td>
+                        <td class="text-nowrap">
+                            <a data-tt="tooltip" data-placement="top" title="Ver detalhes"><i
+                                    class="fa fa-search m-0 p-0 ml-1 mr-1"></i></a>
+                            <a data-tt="tooltip" data-placement="top" title="Editar usuário"><i
+                                    class="fas fa-edit m-0 p-0 ml-1 mr-1"></i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td> <span class="badge badge-danger">inativo</span></td>
+                        <td class="text-nowrap">
+                            <a data-tt="tooltip" data-placement="top" title="Ver detalhes"><i
+                                    class="fa fa-search m-0 p-0 ml-1 mr-1"></i></a>
+                            <a data-tt="tooltip" data-placement="top" title="Editar usuário"><i
+                                    class="fas fa-edit m-0 p-0 ml-1 mr-1"></i></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="clearfix mt-3 mr-3">
+                <ul class="pagination pagination m-0 float-right">
+                    <li class="page-item"><a class="page-link" href="#">«</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">»</a></li>
+                </ul>
+            </div>
+            <br>
         </div>
     </div>
 @stop
