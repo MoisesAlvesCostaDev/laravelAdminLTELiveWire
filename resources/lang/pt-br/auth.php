497e0c73cb673668483c7ecfa7ef304f0288ae4b
Moises Alves Costa <moi.ac@hotmail.com>
2022-02-02 20:09:14 -0300

    feat: traduções

---
+++ resources/lang/pt-br/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'As credenciais informadas não localizadas',
+    'password' => 'Senha informada esta incorreta.',
+    'throttle' => 'Muitas tentativas. Por forvor tente novamente em :seconds segundos.',
+
+];
